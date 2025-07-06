8733a03b251ddaac6de514260cb447ef834eb7d1
sujal1028 <138969035+sujal1028@users.noreply.github.com>
2025-07-06 15:17:11 +0530

    backend and frotend connection error fix in server .js and package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,14 +30,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "./dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../../frontend", "dist", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
 }
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
