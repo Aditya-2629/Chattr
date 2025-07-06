1dd422c080b86e651ca6dae7bcba04f9aa07e418
sujal1028 <138969035+sujal1028@users.noreply.github.com>
2025-07-06 17:37:13 +0530

    onboarding redirect problem

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,19 +1,13 @@
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
-
 const Layout = ({ children, showSidebar = false }) => {
   return (
-    <div className="min-h-screen">
-      <div className="flex">
-        {showSidebar && <Sidebar />}
+    <div className="flex min-h-screen bg-base-100">
+      {showSidebar && <Sidebar />}
 
-        <div className="flex-1 flex flex-col">
-          <Navbar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
 
-          <main className="flex-1 overflow-y-auto">{children}</main>
-        </div>
+        <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
     </div>
   );
 };
-export default Layout;
